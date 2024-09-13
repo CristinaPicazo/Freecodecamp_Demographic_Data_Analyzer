a13d1b664574a1afa6c83edc1025fdad266c2c02
CristinaPicazo <cristina.picazo.merlos@gmail.com>
2024-09-13 20:30:40 +0200

    corrections

--- Freecodecamp_Demographic_Data_Analyzer/demographic_data_analyzer.py
+++ Freecodecamp_Demographic_Data_Analyzer/demographic_data_analyzer.py
@@ -6,8 +6,7 @@ def calculate_demographic_data(print_data=True):
     df = pd.read_csv("./adult.data.csv")
 
     # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
-    race_count = df['race'].count()
-    # race_count = pd.Series(df['race'].value_counts()) //// 
+    race_count = df['race'].value_counts()
 
     # What is the average age of men?
     average_age_men = round(df[df['sex'] == 'Male']['age'].mean(),1)
@@ -31,6 +30,12 @@ def calculate_demographic_data(print_data=True):
 
     # percentage with salary >50K
     higher_education_rich = round(len(df.loc[(df['education'].isin(['Bachelors','Masters','Doctorate'])) & (df['salary'] == '>50K')])* 100 / (len(df['education'])))
+    educacion = len((df['education'] == 'Bachelors') | (df['education'] == 'Masters') | (df['education'] == 'Doctorate'))
+    print('educacion: ', educacion)
+    print('total en educación: ', len(df['education']))
+    print('higher_education_rich: *********************46.5   --', higher_education_rich)
+
+    # higher_education_rich = round(len(df.loc[(df['education'].isin(['Bachelors','Masters','Doctorate'])) & (df['salary'] == '>50K')])* 100 / (len(df['education'])))
 
     # higher_education_rich = round(df[((df["education"]=='Bachelors')|
     #                            (df["education"]=='Masters')|
@@ -57,7 +62,6 @@ def calculate_demographic_data(print_data=True):
     highest_earning_country = df.loc[df['salary'] == '>50K'].max()['native-country']
     highest_earning_country_percentage = sorted(round((df[(df['salary'] == '>50K')]['native-country'].value_counts()/df['native-country'].value_counts()) * 100, 1),reverse=True) [0]
     print('highest_earning_country_percentage: **********', highest_earning_country_percentage)
-    print('NAN: **********', df.loc[df['salary'] == 'nan')
 
     # Identify the most popular occupation for those who earn >50K in India.
     top_IN_occupation = df.loc[df['salary'] == '>50K']['occupation']
@@ -66,15 +70,25 @@ def calculate_demographic_data(print_data=True):
 
     if print_data:
         print("Number of each race:\n", race_count) 
+        # [27816, 3124, 1039, 311, 271]
         print("Average age of men:", average_age_men)
+        # 39.4
         print(f"Percentage with Bachelors degrees: {percentage_bachelors}%")
+        # 16.4
         print(f"Percentage with higher education that earn >50K: {higher_education_rich}%")
+        # 46.5
         print(f"Percentage without higher education that earn >50K: {lower_education_rich}%")
+        # 17.4
         print(f"Min work time: {min_work_hours} hours/week")
+        # 1
         print(f"Percentage of rich among those who work fewest hours: {rich_percentage}%")
+        # 10
         print("Country with highest percentage of rich:", highest_earning_country)
+        # 'Iran'
         print(f"Highest percentage of rich people in country: {highest_earning_country_percentage}%")
+        # 41.9
         print("Top occupations in India:", top_IN_occupation)
+        # 'Prof-specialty'
 
     return {
         'race_count': race_count,
